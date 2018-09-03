ef8ea62626dbb853892b80763e65354470fed39d
Stefan Lazarevic <stefanlazarevic.contact@gmail.com>
2018-09-03 23:56:25 +0200

    Improved content, added work route.

---
+++ src/content/en/pages/Work.js
@@ -0,0 +1,15 @@
+import Routes from "../../../constants/Routes";
+
+export default {
+    PAGE_TITLE: 'My work',
+    PAGE_NAVIGATION: {
+        BACK_BUTTON_TEXT: 'Back to about',
+        BACK_BUTTON_LINK: Routes.ABOUT,
+        FORWARD_BUTTON_TEXT: 'Go to experience',
+        FORWARD_BUTTON_LINK: Routes.EXPERIENCE
+    },
+    SEO: {
+        DESCRIPTION: '',
+        KEYWORDS: '',
+    }
+};
---
+++ src/pages/work/work.page.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+import env from '../../config/env.client';
+import ContentController from '../../content/ContentController';
+import PageHeader from '../../components/pageHeader/PageHeader';
+import PageNavigation from '../../components/pageNavigation/PageNavigation';
+
+class WorkPage extends React.Component {
+
+    constructor() {
+        super();
+        this.Content = {};
+    }
+
+    componentWillMount() {
+        this.Content = ContentController.getContentForLanguage('en').work;
+    }
+
+    head() {
+        return (
+            <Helmet>
+                <title>{env.APP_BASE_TITLE} | {this.Content.PAGE_TITLE}</title>
+                <meta name="description" content={this.Content.SEO.DESCRIPTION} />
+                <meta name="keywords" content="stefan, developer, web" />
+                <style>{this.Content.CRITICAL_CSS}</style>
+                <link rel="canonical" href={env.APP_BASE_URL} />
+            </Helmet>
+        )
+    }
+
+    render() {
+        return (
+            <section id="about">
+                {this.head()}
+                <PageHeader />
+                <div id="work-main">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12">
+                                <h2 className="page__title text--center text-md--left">{this.Content.PAGE_TITLE}</h2>
+                            </div>
+                        </div>
+                        <PageNavigation prevLink={this.Content.PAGE_NAVIGATION.BACK_BUTTON_LINK}
+                            prevText={this.Content.PAGE_NAVIGATION.BACK_BUTTON_TEXT}
+                            nextLink={this.Content.PAGE_NAVIGATION.FORWARD_BUTTON_LINK}
+                            nextText={this.Content.PAGE_NAVIGATION.FORWARD_BUTTON_TEXT}
+                        />
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default WorkPage;
