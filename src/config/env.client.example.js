d710d0609e90f63567c5993ee0183c533c39000f
Stefan Lazarevic <stefanlazarevic.contact@gmail.com>
2018-09-08 11:07:34 +0200

    Config added

--- src/config/env.client.example.js
+++
@@ -1,5 +0,0 @@
-export default {
-    APP_AUTHOR: 'Stefan Lazarevic',
-    APP_BASE_TITLE: 'My new website',
-    APP_BASE_URL: 'http://localhost:80',
-}
---
+++ src/config/env.client.js
@@ -0,0 +1,5 @@
+export default {
+    APP_AUTHOR: 'Stefan Lazarevic',
+    APP_BASE_TITLE: 'Stefan Lazarevic - Full Stack Web Developer and Javascript Engineer',
+    APP_BASE_URL: 'https://stefanlazarevic-website.herokuapp.com/',
+}
